3a5fb25cbc967dc2ad207399b00d048721f7fa45
王唯 <bbww525@163.com>
2019-11-23 21:57:11 +0800

    添加了NewsViewController、TestViewController 修改了Main.storyboard

---
+++ GitProject/NewsViewController.swift
@@ -0,0 +1,33 @@
+//
+//  NewsViewController.swift
+//  GitProject
+//
+//  Created by wangwei on 2019/11/23.
+//  Copyright © 2019 零点网络. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func show() {
+        print("ok")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
